83afe89a7d407e6f9b30709291df378f299dcc1d
Collince-Okeyo <collinceokeyo98@gmail.com>
2021-09-12 22:54:53 +0300

    Completed LeakCanary Article

--- app/src/main/java/com/ramgdeveloper/leakcanary/LeakCanaryApp.kt
+++ app/src/main/java/com/ramgdeveloper/leakcanary/LeakCanaryApp.kt
@@ -3,9 +3,9 @@ package com.ramgdeveloper.leakcanary
 import android.app.Application
 import leakcanary.AppWatcher
 
-class LeakCanaryApp : Application(){
+class LeakCanaryApp: Application() {
     override fun onCreate() {
         super.onCreate()
-        AppWatcher.config = AppWatcher.config.copy(watchActivities = false)
+        val objectWatcher = AppWatcher.objectWatcher
     }
 }
--- app/src/main/java/com/ramgdeveloper/leakcanary/MainActivity.kt
+++ app/src/main/java/com/ramgdeveloper/leakcanary/MainActivity.kt
@@ -2,10 +2,24 @@ package com.ramgdeveloper.leakcanary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.graphics.drawable.Drawable
 
 class MainActivity : AppCompatActivity() {
+
+    private var myBackground: Drawable? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        val textView = TextView(this)
+        textView.text = "Memory leaks are not good to applications"
+        if (myBackground == null) {
+            myBackground = getDrawable(R.drawable.ic_launcher_background)
+            //To avoid leak use the code below:
+            //mBackground = getApplicationContext().getResources().getDrawable(R.drawable.ic_launcher_background);
+        }
+        textView.setBackgroundDrawable(myBackground)
+        setContentView(textView)
     }
 }
+
